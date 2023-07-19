6e5cb6dc885c39f7628150f0f7e0221f9d0d2229
Abdur Rehman <abdurrehmanmughal1@gmail.com>
2023-07-19 11:13:35 +0500

    feat: string to integer

---
+++ stringToInteger/stringToInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringToInteger
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string applesInBasket = "12";
+            string baskets = "4";
+
+            int total = Convert.ToInt32(applesInBasket) * Convert.ToInt32(baskets);
+
+            string msg = $"There are total of {total} apples";
+            Console.WriteLine(msg);
+        }
+    }
+}
